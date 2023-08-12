3ef18b4a10db334f373a3a314415d4ac3ca9450c
herostoky <72009671+herostoky@users.noreply.github.com>
2023-08-12 06:23:46 +0000

    add static middleware to serve assets

--- myApp.js
+++ myApp.js
@@ -1,9 +1,19 @@
 let express = require('express');
+let path = require('path');
 console.log("Hello World");
 let app = express();
 
+// Middleware
+app.use(express.static(__dirname + "/public"));
+// app.use(express.static(path.join(__dirname, "public")));
 
+app.get("/hello", function(req, res) {
+  res.send("Hello Express");
+});
 
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 
 
